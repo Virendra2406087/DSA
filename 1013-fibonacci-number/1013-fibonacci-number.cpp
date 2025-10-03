ca6598a6483f4a888a87c7e0d3fe9f459c4b965e
Virendra2406087 <virendrakumar993177@gmail.com>
2025-10-03 22:55:48 +0530

    Time: 14 ms (22.31%), Space: 7.8 MB (63.57%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.cpp
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -1,13 +1,18 @@
 class Solution {
 public:
-    int fib(int n) {
+    int fibUsingRec(int n){
         if(n==0){
             return 0;
         }
         if(n==1){
             return 1;
         }
-        int  ans=fib(n-1)+fib(n-2);
+        int  ans=fibUsingRec(n-1)+fibUsingRec(n-2);
+        return ans;
+    }
+    int fib(int n) {
+        
+        int  ans=fibUsingRec(n);
         return ans;
     }
 };
