6e24eda9751c19314295213c8971e06a92aae067
Virendra2406087 <virendrakumar993177@gmail.com>
2025-10-08 23:56:38 +0530

    Time: 0 ms (100%), Space: 10.7 MB (46.6%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,18 +1,31 @@
 class Solution {
 public:
-    int solve(vector<int>& nums, int index, vector<int>& dp) {
-        if (index >= nums.size()) return 0;
-        if (dp[index] != -1) return dp[index];
-
-        int includeans = nums[index] + solve(nums, index + 2, dp);
-        int excludeans = solve(nums, index + 1, dp);
-
-        return dp[index] = max(includeans, excludeans);
+    int solveUsingRec(vector<int>&nums,int index){
+        if(index>=nums.size()){
+            return 0;
+        }
+        int includeAns=nums[index]+solveUsingRec(nums,index+2);
+        int excludeAns=0+solveUsingRec(nums,index+1);
+        int ans=max(includeAns,excludeAns);
+        return ans;
     }
-
+     int solveUsingMem(vector<int>&nums,int index,vector<int>& dp){
+        if(index>=nums.size()){
+            return 0;
+        }
+        if(dp[index] !=-1){
+            return dp[index];
+        }
+        int includeAns=nums[index]+solveUsingMem(nums,index+2,dp);
+        int excludeAns=0+solveUsingMem(nums,index+1,dp);
+        int ans=max(includeAns,excludeAns);
+        dp[index]=ans;
+        return dp[index];
+    }
     int rob(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> dp(n, -1);
-        return solve(nums, 0, dp);
+        int index=0;
+        vector<int>dp(nums.size()+1,-1);
+        int ans=solveUsingMem(nums,index,dp);
+        return ans;
     }
 };
