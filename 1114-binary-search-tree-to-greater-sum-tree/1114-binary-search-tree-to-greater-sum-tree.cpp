7b26ae035881fc5ca10defd88e0e2a4da929c33c
Virendra2406087 <virendrakumar993177@gmail.com>
2025-11-04 19:11:49 +0530

    Time: 0 ms (100%), Space: 11.3 MB (47.13%) - LeetHub

---
+++ 1114-binary-search-tree-to-greater-sum-tree/1114-binary-search-tree-to-greater-sum-tree.cpp
@@ -0,0 +1,56 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void inorderstore(TreeNode* root,vector<int>&inOrder){
+        if(root==NULL){
+            return;
+        }
+        inorderstore(root->left,inOrder);
+        inOrder.push_back(root->val);
+        inorderstore(root->right,inOrder);
+        return;
+    }
+    void updateTree(TreeNode* root,vector<int>&inOrder,int &index){
+        if(root==NULL){
+            return;
+        }
+        updateTree(root->left,inOrder,index);
+        root->val=inOrder[index];
+        index++;
+        updateTree(root->right,inOrder,index);
+        return;
+    }
+    TreeNode* bstToGst(TreeNode* root) {
+        if(root==NULL){
+            return NULL;
+        }
+        if(root->left ==NULL && root->right ==NULL){
+            return root;
+        }
+        vector<int>inOrder;
+        inorderstore(root,inOrder);
+        int n=inOrder.size();
+        for(int i=n-1;i>=0;i--){
+            int currVal=inOrder[i];
+            int nextVal=0;
+            if(i+1<n){
+                nextVal=inOrder[i+1];
+            }
+            int sum=currVal+nextVal;
+            inOrder[i]=sum;
+        }
+        int index=0;
+        updateTree(root,inOrder,index);
+        return root;
+    }
+};
