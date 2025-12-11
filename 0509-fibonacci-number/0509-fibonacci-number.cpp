11eac3bb88110c27383189ccc70c3c4076923df6
Virendra2406087 <virendrakumar993177@gmail.com>
2025-12-11 22:55:06 +0530

    Time: 0 ms (100%), Space: 8.1 MB (20.28%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -10,8 +10,22 @@ public:
         int ans=solveUsingRec(n-1)+solveUsingRec(n-2);
         return ans;
     }
+    int solveUsingMem(int n,vector<int>&dp){
+        if(n==0 || n==1){
+            return n;
+        }
+        if(dp[n] !=-1){
+            return dp[n];
+        }
+        int ans=solveUsingMem(n-1,dp)+solveUsingMem(n-2,dp);
+        dp[n]=ans;
+        return dp[n];
+        
+    }
+
     int fib(int n) {
-        int ans=solveUsingRec(n);
+        vector<int>dp(n+1,-1);
+        int ans=solveUsingMem(n,dp);
         return ans;
     }
 };
