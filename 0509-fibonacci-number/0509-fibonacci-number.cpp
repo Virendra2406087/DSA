b1c6a3776163280cb0b8672b6923f69ca7e46297
Virendra2406087 <virendrakumar993177@gmail.com>
2025-12-14 22:15:41 +0530

    Time: 0 ms (100%), Space: 7.7 MB (64.44%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -17,11 +17,40 @@ public:
         int ans=solveUsingMem(n-1,dp)+solveUsingMem(n-2,dp);
         dp[n]=ans;
         return dp[n];
-
+    }
+    int solveUsingTab(int n){
+        vector<int>dp(n+1,-1);
+        if(n==0 || n==1){
+            return n;
+        }
+        dp[0]=0;
+        dp[1]=1;
+        for(int i=2;i<=n;i++){
+            int ans=dp[i-1]+dp[i-2];
+            dp[i]=ans;
+        }
+        return dp[n];
+        
+    }
+    int solveUsingTabSO(int n){
+        // vector<int>dp(n+1,-1);
+        if(n==0 || n==1){
+            return n;
+        }
+        int prev2=0;
+        int prev1=1;
+        int curr=-1;
+        for(int i=2;i<=n;i++){
+            curr=prev1+prev2;
+            prev2=prev1;
+            prev1=curr;
+        }
+        return curr;
+        
     }
     int fib(int n) {
-        vector<int>dp(n+1,-1);
-        int ans=solveUsingMem(n,dp);
+        // vector<int>dp(n+1,-1);
+        int ans=solveUsingTabSO(n);
         return ans;
     }
 };
