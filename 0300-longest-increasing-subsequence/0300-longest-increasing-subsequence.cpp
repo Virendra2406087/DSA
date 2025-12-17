10d72728422eea00e861dfe03beb106d2e449134
Virendra2406087 <virendrakumar993177@gmail.com>
2025-12-17 22:48:19 +0530

    Time: 282 ms (29.35%), Space: 296.5 MB (18.99%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -30,13 +30,31 @@ public:
         dp[curr][prev+1]=finalAns;
         return dp[curr][prev+1];
     }
+    int solveUsingTab(vector<int>& nums){
+        int n=nums.size();
+        vector<vector<int>>dp(n+1,vector<int>(n+1,0));
+        // if(dp[curr][prev+1] !=-1){
+        //    return dp[curr][prev+1];
+        // }
+        for(int curr=n-1;curr>=0;curr--){
+            for(int prev=curr-1;prev>=-1;prev--){
+            int includeAns=0;
+            if(prev ==-1 || nums[curr]>nums[prev]){
+            includeAns=1+dp[curr+1][curr+1];
+        }
+            int excludeAns=0+dp[curr+1][prev+1];
+            int finalAns=max(includeAns,excludeAns);
+            dp[curr][prev+1]=finalAns;
+        }
+        }
+        return dp[0][0];
+    }
 
     int lengthOfLIS(vector<int>& nums) {
-        int n=nums.size();
-        vector<vector<int>>dp(n,vector<int>(n+1,-1));
+        // vector<vector<int>>dp(n,vector<int>(n+1,-1));
         int curr=0;
         int prev=-1;
-        int ans=solveUsingMem(nums,curr,prev,dp);
+        int ans=solveUsingTab(nums);
         return ans;
     }
 };
