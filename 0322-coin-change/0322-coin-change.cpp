ef389fe766ac11a56444781ea779d8c701dbfdb3
Virendra2406087 <virendrakumar993177@gmail.com>
2025-12-17 20:56:38 +0530

    Time: 19 ms (79.34%), Space: 22.1 MB (43.68%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -37,11 +37,27 @@ public:
         dp[amount]=mini;
         return dp[amount];
     }
-
+    int solveUsingTab(vector<int>& coins, int amount){
+        vector<int>dp(amount+1,-1);
+        dp[0]=0;
+        for(int value=1;value<=amount;value++){
+            int mini=INT_MAX;
+        for(int i=0;i<coins.size();i++){
+            if(coins[i]<=value){
+              int recursionAns= dp[value-coins[i]];
+              if(recursionAns !=INT_MAX){
+                mini=min(1+recursionAns,mini);
+              }
+            }
+           dp[value]=mini;
+        }
+     }
+        return dp[amount];
+    }
     int coinChange(vector<int>& coins, int amount) {
         int n=coins.size();
         vector<int>dp(amount+1,-1);
-        int ans=solveUsingMem(coins,amount,dp);
+        int ans=solveUsingTab(coins,amount);
         if(ans==INT_MAX){
             return -1;
         }else{
