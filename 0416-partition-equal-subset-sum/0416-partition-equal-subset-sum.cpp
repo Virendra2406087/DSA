1038b9cd001fd43e22097edbe048566db9079102
Virendra2406087 <virendrakumar993177@gmail.com>
2025-12-20 00:30:31 +0530

    Time: 115 ms (63.55%), Space: 112 MB (13.34%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -35,6 +35,44 @@ public:
         dp[index][target]=ans;
         return dp[index][target];
     }
+    bool solveUsingTab(vector<int> & nums,int index,int target){
+        int n=nums.size();
+        if(index>=n){
+          return 0;
+        }
+        if(target<0){
+            return 0;
+        }
+        if(target==0){
+            return 1;
+        }
+        
+        vector<vector<int>> dp(n, vector<int>(target + 1, -1));
+        bool include=dp[index+1][target-nums[index]];
+        bool exclude=dp[index+1][target];
+        bool ans= (include || exclude);
+        dp[index][target]=ans;
+        return dp[index][target];
+    }
+    bool solveUsingTabSO(vector<int>& nums,int index,int target){
+        int n=nums.size();
+        vector<int>curr(target+1,0);
+        vector<int>next(target+1,0);
+        curr[0]=1;
+        next[0]=1;
+        for(int index=n-1;index>=0;index--){
+            for(int t=1;t<=target;t++){
+                bool include=0;
+                if(t-nums[index]>=0){
+                    include=next[t-nums[index]];
+                }
+                bool exclude=next[t];
+                curr[t]=(include || exclude);
+            }
+            next=curr;
+        }
+        return next[target];
+    }
     bool canPartition(vector<int>& nums) {
         int n=nums.size();
        
@@ -48,7 +86,7 @@ public:
         int target=sum/2;
         int index=0;
         vector<vector<int>> dp(n, vector<int>(target + 1, -1));
-        bool ans=solveUsingMem(nums,index,target,dp);
+        bool ans=solveUsingTabSO(nums,index,target);
         return ans;
     }
 };
