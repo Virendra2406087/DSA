b7797ca269426fa186dd47d23974121f09fe1b8d
Virendra2406087 <virendrakumar993177@gmail.com>
2025-12-20 23:15:21 +0530

    Time: 85 ms (22.76%), Space: 11.2 MB (21.74%) - LeetHub

---
+++ 0375-guess-number-higher-or-lower-ii/0375-guess-number-higher-or-lower-ii.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int solveUsingRec(int start,int end){
+        if(start>=end){
+            return 0;
+        }
+        int ans=INT_MAX;
+        for(int i=start;i<=end;i++){
+            ans=min(ans,i+max(solveUsingRec(start,i-1),solveUsingRec(i+1,end)));
+        }
+        return ans;
+    }
+    int solveUsingMem(int start,int end,vector<vector<int>> &dp){
+        if(start>=end){
+            return 0;
+        }
+        if(dp[start][end] !=-1){
+            return dp[start][end];
+        }
+        int ans=INT_MAX;
+        for(int i=start;i<=end;i++){
+            ans=min(ans,i+max(solveUsingMem(start,i-1,dp),solveUsingMem(i+1,end,dp)));
+        }
+        dp[start][end]=ans;
+        return dp[start][end];
+    }
+    int getMoneyAmount(int n) {
+        vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
+        int ans=solveUsingMem(1,n,dp);
+        return ans;
+    }
+};
