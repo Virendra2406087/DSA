03a922515fbaf2582906cfb1883f365148f0a562
Virendra2406087 <virendrakumar993177@gmail.com>
2026-01-02 22:36:54 +0530

    Time: 7 ms (6.7%), Space: 9.9 MB (25.64%) - LeetHub

---
+++ 1006-clumsy-factorial/1006-clumsy-factorial.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int clumsy(int n) {
+        stack<int>st;
+        st.push(n);
+        n--;
+        int op=0;  // 0=*, 1=/, 2=+,3=-
+        while(n>0){
+            if(op==0){
+                int top=st.top();
+                st.pop();
+                st.push(top*n);
+            }
+            else if(op==1){
+                int top=st.top();
+                st.pop();
+                st.push(top/n);
+            }
+            else if(op==2){
+                st.push(+n);
+            }
+            else{
+                
+                st.push(-n);
+            }
+            op = (op + 1) % 4;
+               n--;
+        }
+        int result=0;
+        while(!st.empty()){
+            result=result+st.top();
+            st.pop();
+        }
+        return result;
+
+    }
+};
