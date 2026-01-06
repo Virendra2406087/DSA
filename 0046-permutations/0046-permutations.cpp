4ed808a660797430a4ba8b4385dcc586c2439eda
Virendra2406087 <virendrakumar993177@gmail.com>
2026-01-06 19:18:43 +0530

    Time: 5 ms (7.91%), Space: 12.6 MB (8.27%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,24 +1,24 @@
 class Solution {
 public:
-    void printallPermutation(vector<int>&nums,int i,vector<vector<int>>& ans){
-        if(i>=nums.size()){
-            ans.push_back(nums);
-            return ;
-        }
-        unordered_set<int>used;
-        for(int j=i;j<nums.size();j++){
-            if(used.count(nums[j])){
-                continue;
-            }
-           used.insert(nums[j]);
-           swap(nums[i],nums[j]);
-           printallPermutation(nums,i+1,ans);
-           swap(nums[i],nums[j]);
-        }
+void printPermutation(vector<int>& nums,int i,vector<vector<int>> &ans){
+    if(i>=nums.size()){
+        ans.push_back(nums);
     }
+    unordered_set<int>used;
+    for(int j=i;j<nums.size();j++){
+       if(used.count(nums[j])){
+        continue;
+       }
+       used.insert(nums[i]);
+       swap(nums[i],nums[j]);
+       printPermutation(nums,i+1,ans);
+       swap(nums[i],nums[j]);
+    }
+
+}
     vector<vector<int>> permute(vector<int>& nums) {
         vector<vector<int>>ans;
-        printallPermutation(nums,0,ans);
+        printPermutation(nums,0,ans);
         return ans;
     }
 };
