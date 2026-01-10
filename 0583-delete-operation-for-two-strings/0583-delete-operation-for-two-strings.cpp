b9583758b57641da44dc6852879cbfc8c9f9cb25
Virendra2406087 <virendrakumar993177@gmail.com>
2026-01-10 19:18:29 +0530

    Time: 19 ms (14.05%), Space: 17.1 MB (17.9%) - LeetHub

---
+++ 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+     int solve(string &s1,string &s2,int i,int j){
+        int m=s1.length();
+        int n=s2.length();
+        if(i==m){
+            return n-j;
+        }
+        if(j==n){
+            return m-i;
+        }
+        if(s1[i]==s2[j]){
+            return solve(s1,s2,i+1,j+1);
+        }
+        
+        int deleted1=1+solve(s1,s2,i+1,j);
+        int deleted2=1+solve(s1,s2,i,j+1);
+
+        return min(deleted1,deleted2);
+    }
+    int solveMem(string &s1,string &s2,int i,int j,vector<vector<int>> &dp){
+        int m=s1.length();
+        int n=s2.length();
+        if(i==m){
+            return n-j;
+        }
+        if(j==n){
+            return m-i;
+        }
+        if(dp[i][j] != -1){
+            return dp[i][j];
+        }
+        if(s1[i]==s2[j]){
+            return solveMem(s1,s2,i+1,j+1,dp);
+        }
+        
+        int deleted1=1+solveMem(s1,s2,i+1,j,dp);
+        int deleted2=1+solveMem(s1,s2,i,j+1,dp);
+
+        dp[i][j] = min(deleted1,deleted2);
+        return dp[i][j];
+    }
+    int minDistance(string word1, string word2) {
+        int m=word1.length();
+        int n=word2.length();
+        vector<vector<int>>dp(m+1,vector<int>(n+1,-1));
+        int ans=solveMem(word1,word2,0,0,dp);
+        return ans;
+    }
+};
