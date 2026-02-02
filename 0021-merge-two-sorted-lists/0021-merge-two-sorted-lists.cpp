67d8b178002f38d49a5ad8adbe407e53d6422343
Virendra2406087 <virendrakumar993177@gmail.com>
2026-02-02 18:29:50 +0530

    Time: 0 ms (100%), Space: 19.4 MB (61.68%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -8,56 +8,47 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-
-        if(list1 == NULL){
-           return list2;
+        if(list1==nullptr){
+            return list2;
         }
-        if(list2 == NULL){
-          return list1;
-        } 
+        if(list2==nullptr){
+            return list1;
+        }
+        ListNode* head=nullptr;
+        ListNode* tail=nullptr;
 
-        ListNode* head = NULL;
-        ListNode* tail = NULL;
-
-        ListNode* temp1 = list1;
-        ListNode* temp2 = list2;
-
-        while(temp1 != NULL && temp2 != NULL){
-
+        ListNode* temp1=list1;
+        ListNode* temp2=list2;
+        while(temp1 != nullptr && temp2 != nullptr){
             ListNode* node;
-            if(temp1->val < temp2->val){
-                node = temp1;
-                temp1 = temp1->next;
-            } else {
-                node = temp2;
-                temp2 = temp2->next;
+            if(temp1->val<=temp2->val){
+                node=temp1;
+                temp1=temp1->next;
+            }else{
+                node=temp2;
+                temp2=temp2->next;
             }
-
-            if(head == NULL){
-                head = node;
-                tail = node;
-            } else {
-                tail->next = node;
-                tail = node;
+            if(head==nullptr){
+                head=node;
+                tail=node;
+            }else{
+                tail->next=node;
+                tail=node;
             }
         }
-
-        while(temp1 != NULL){
-            tail->next = temp1;
-            tail = temp1;
-            temp1 = temp1->next;
+        while(temp1 != nullptr){
+            tail->next=temp1;
+            tail=temp1;
+            temp1=temp1->next;
         }
-
-        while(temp2 != NULL){
-            tail->next = temp2;
-            tail = temp2;
-            temp2 = temp2->next;
+        while(temp2 != nullptr){
+            tail->next=temp2;
+            tail=temp2;
+            temp2=temp2->next;
         }
-
         return head;
     }
 };
