722c44f15d5ed6741abdc036c7ea67d17b4947bb
Virendra2406087 <virendrakumar993177@gmail.com>
2026-02-10 20:07:41 +0530

    Time: 1048 ms (5.04%), Space: 60.7 MB (5.53%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -1,60 +1,16 @@
-// class Solution {
-// public:
-// //    void printPermutation(string &s1, int i, vector<string> &ans) {
-// //     if (i == s1.size()) {
-// //         ans.push_back(s1);
-// //         return;
-// //     }
-
-// //     unordered_set<char> used;
-
-// //     for (int j = i; j < s1.size(); j++) {
-// //         if (used.count(s1[j])){
-// //         continue;
-// //         } 
-
-// //         used.insert(s1[j]);
-// //         swap(s1[i], s1[j]);
-// //         printPermutation(s1, i + 1, ans);
-// //         swap(s1[i], s1[j]); 
-// //     }
-// // }
-
-
-//     bool checkInclusion(string s1, string s2) {
-//         if(s1.length() > s2.length()){
-//             return false;
-//         }
-//         vector<string>ans;
-//         printPermutation(s1,0,ans);
-
-//         int k = s1.size();
-
-//         for (auto &perm : ans) {
-//             for (int i = 0; i <= s2.size() - k; i++) {
-//                if (s2.substr(i, k) == perm) {
-//                   return true;
-//                 }
-//             }
-//         }
-//         return false;
-//     }
-// };
-
-
 class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
-        if (s1.size() > s2.size()) return false;
-
-        sort(s1.begin(), s1.end());
-        int k = s1.size();
-
-        for (int i = 0; i <= s2.size() - k; i++) {
-            string temp = s2.substr(i, k);
-            sort(temp.begin(), temp.end());
-
-            if (temp == s1) {
+        int n=s1.length();
+        int m=s2.length();
+        if(n>m){
+            return false;
+        }
+        sort(s1.begin(),s1.end());
+        for(int i=0;i<=m-n;i++){
+            string substring=s2.substr(i,n);
+            sort(substring.begin(),substring.end());
+            if(s1==substring){
                 return true;
             }
         }
