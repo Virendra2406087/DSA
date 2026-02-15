9d0c32b83d7178ca99d7eccb9ba5e0187fb6ebe9
Virendra2406087 <virendrakumar993177@gmail.com>
2026-02-15 19:02:42 +0530

    Time: 1 ms (78.79%), Space: 11.6 MB (23.8%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -1,65 +1,59 @@
 class Solution {
 public:
-    bool isSafe(int row,int col,vector<vector<char>>& board){
-        int n=board.size();
-        for(int i=0;i<n;i++){
-            if(board[row][i]=='Q'){
+    bool isSafe(int row, int col, vector<vector<char>>& board) {
+        int n = board.size();
+
+        // check left side
+        for (int i = 0; i < col; i++) {
+            if (board[row][i] == 'Q')
                 return false;
-            }
         }
-        // Upper diagonal
-        int i=row-1;
-        int j=col-1;
-        while(i>=0 && j>=0){
-            if(board[i][j]=='Q'){
+
+        // upper-left diagonal
+        for (int i = row, j = col; i >= 0 && j >= 0; i--, j--) {
+            if (board[i][j] == 'Q')
                 return false;
-            }
-            i--;
-            j--;
         }
-        // Lower diagonal
-        int x=row+1;
-        int y=col-1;
-        while(x<n && y>=0){
-            if(board[x][y]=='Q'){
+
+        // lower-left diagonal
+        for (int i = row, j = col; i < n && j >= 0; i++, j--) {
+            if (board[i][j] == 'Q')
                 return false;
-            }
-              x++;
-              y--;
         }
+
         return true;
     }
-    void storeSolution(vector<vector<char>>& board,vector<vector<string>> & ans,int n){
-        vector<string>temp;
-        for(int row=0;row<n;row++){
-            string output="";
-            for(int col=0;col<n;col++){
-                char ch=board[row][col];
-                output.push_back(ch);
-            }
-                temp.push_back(output);
+
+    void storeSolution(vector<vector<char>>& board,
+                       vector<vector<string>>& ans, int n) {
+        vector<string> temp;
+        for (int i = 0; i < n; i++) {
+            string row(board[i].begin(), board[i].end());
+            temp.push_back(row);
         }
         ans.push_back(temp);
-
     }
-    void solve(vector<vector<char>>& board,vector<vector<string>>& ans,int n,int col){
-        if(col>=n){
-            storeSolution(board,ans,n);
-            return ;
+
+    void solve(vector<vector<char>>& board,
+               vector<vector<string>>& ans, int n, int col) {
+        if (col == n) {
+            storeSolution(board, ans, n);
+            return;
         }
-        for(int row=0;row<n;row++){
-            if(isSafe(row,col,board)){
-                board[row][col]='Q';
-                solve(board,ans,n,col+1);
-                board[row][col]='.';
+
+        for (int row = 0; row < n; row++) {
+            if (isSafe(row, col, board)) {
+                board[row][col] = 'Q';
+                solve(board, ans, n, col + 1);
+                board[row][col] = '.';
             }
         }
     }
+
     vector<vector<string>> solveNQueens(int n) {
-        vector<vector<string>>ans;
-        vector<vector<char>>board(n,vector<char>(n,'.'));
-        int col=0;
-        solve(board,ans,n,col);
+        vector<vector<string>> ans;
+        vector<vector<char>> board(n, vector<char>(n, '.'));
+        solve(board, ans, n, 0);
         return ans;
     }
 };
