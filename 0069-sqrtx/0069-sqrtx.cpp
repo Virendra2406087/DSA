ee081d19e1e7b0a18157b1abeec457720c78cf55
Virendra2406087 <virendrakumar993177@gmail.com>
2026-02-16 19:43:33 +0530

    Time: 0 ms (100%), Space: 8.6 MB (13.26%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -1,33 +1,24 @@
 class Solution {
 public:
-    bool isValid(int x,long long mid){
-        long long product=mid*mid;
-        if(product==x){
-            return true;
-        }else{
-            return false;
-        }
-    }
     int mySqrt(int x) {
-        if(x==0 || x==1){
-            return x;
-        }
+        int target=x;
         int s=0;
-        int e=x;
+        int e=target;
         int ans=-1;
         while(s<=e){
-            int mid=s+(e-s)/2;
-            long long product=1LL*mid*mid;
-            if(isValid(x,mid)){
+            long long int mid=s+(e-s)/2;
+            long long int prod=mid*mid;
+            if(prod==target){
                 return mid;
             }
-            if(product<x){
+            if(prod>target){
+                e=mid-1;
+            }
+            if(prod<target){
                 ans=mid;
                 s=mid+1;
-            }else{
-                e=mid-1;
             }
         }
         return ans;
-    }
+        }
 };
