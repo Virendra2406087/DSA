52a4e056dc1cf016772eb05f06078c013af9bf60
Virendra2406087 <virendrakumar993177@gmail.com>
2026-03-12 23:25:21 +0530

    Time: 2 ms (47.01%), Space: 20.9 MB (88.41%) - LeetHub

--- 0113-path-sum-ii/0113-path-sum-ii.cpp
+++ 0113-path-sum-ii/0113-path-sum-ii.cpp
@@ -11,27 +11,27 @@
  */
 class Solution {
 public:
-   void solve(TreeNode* root,int targetSum,int sum,vector<int>temp,vector<vector<int>>&ans){
-    if(root==nullptr){
-        return ;
-    }
-    sum=sum+root->val;
-    temp.push_back(root->val);
-    if(root->left ==nullptr && root->right==nullptr){
-        if(sum==targetSum){
-           ans.push_back(temp);
-           temp.pop_back();
-           return;
+    void solve(TreeNode* root,int target,int sum,vector<int>& temp,vector<vector<int>>&ans){
+        if(root==nullptr){
+            return;
         }
+        sum=sum+root->val;
+        temp.push_back(root->val);
+        if(root->left==nullptr && root->right==nullptr){
+            if(sum==target){
+                ans.push_back(temp);
+                temp.pop_back();
+                return;
+            }
+        }
+        solve(root->left,target,sum,temp,ans);
+        solve(root->right,target,sum,temp,ans);
+        temp.pop_back();
     }
-    solve(root->left,targetSum,sum,temp,ans);
-    solve(root->right,targetSum,sum,temp,ans);
-    temp.pop_back();
-   }
     vector<vector<int>> pathSum(TreeNode* root, int targetSum) {
-        int sum=0;
         vector<int>temp;
         vector<vector<int>>ans;
+        int sum=0;
         solve(root,targetSum,sum,temp,ans);
         return ans;
     }
