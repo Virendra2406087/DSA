301b969ea23af92eeeab9088f0b405a95a7306cb
Virendra2406087 <virendrakumar993177@gmail.com>
2026-03-18 22:23:16 +0530

    Time: 0 ms (100%), Space: 17.1 MB (44.24%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
@@ -6,44 +6,36 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right)
- *         : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-
 class Solution {
 public:
     vector<vector<int>> levelOrder(TreeNode* root) {
-        vector<vector<int>> ans;
-        if (root == nullptr) {
+        vector<vector<int>>ans;
+        if(root==nullptr){
             return ans;
         }
-
-        queue<TreeNode*> q;
+        queue<TreeNode*>q;
         q.push(root);
         q.push(nullptr);
-
-        vector<int> level;
-
-        while (!q.empty()) {
-            TreeNode* front = q.front();
+        vector<int>level;
+        while(!q.empty()){
+            TreeNode* frontNode=q.front();
             q.pop();
-
-            if (front == nullptr) {
+            if(frontNode==nullptr){
                 ans.push_back(level);
                 level.clear();
-
-                if (!q.empty()) {
+                if(!q.empty()){
                     q.push(nullptr);
                 }
-            } else {
-                level.push_back(front->val);
-
-                if (front->left != nullptr) {
-                    q.push(front->left);
+            }else{
+                level.push_back(frontNode->val);
+                if(frontNode->left != nullptr){
+                    q.push(frontNode->left);
                 }
-                if (front->right != nullptr) {
-                    q.push(front->right);
+                if(frontNode->right != nullptr){
+                    q.push(frontNode->right);
                 }
             }
         }
