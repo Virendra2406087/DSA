bfe0a4e6579590322520153dc676b5ac66a4417a
Virendra2406087 <virendrakumar993177@gmail.com>
2026-03-19 20:11:35 +0530

    Time: 2 ms (58.67%), Space: 37.3 MB (46.89%) - LeetHub

--- 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.cpp
+++ 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.cpp
@@ -11,36 +11,37 @@
  */
 class Solution {
 public:
-    void inorder(TreeNode* root,vector<int>&ans){
-        if(root==NULL){
+    void inorder(TreeNode* root,vector<int>& ans){
+        if(root==nullptr){
             return;
         }
         inorder(root->left,ans);
         ans.push_back(root->val);
         inorder(root->right,ans);
-        return;
     }
-    bool findTarget(TreeNode* root, int k) {
-        if(root==NULL){
-            return false;
-        }
-        vector<int>ans;
-        inorder(root,ans);
+    bool checkTwoSum(vector<int>& ans,int k){
+        int n=ans.size();
         int s=0;
-        int e=ans.size()-1;
+        int e=n-1;
         while(s<e){
             int sum=ans[s]+ans[e];
             if(sum==k){
                 return true;
-            }else{
-                if(sum>k){
-                    e--;
-                }
-                if(sum<k){
-                    s++;
-                }
             }
+            if(sum>k){
+                e--;
+            }
+            if(sum<k){
+                s++;
+            }
+
         }
         return false;
+    }
+    bool findTarget(TreeNode* root, int k) {
+        vector<int>ans;
+        inorder(root,ans);
+        bool result=checkTwoSum(ans,k);
+        return result;
     }
 };
