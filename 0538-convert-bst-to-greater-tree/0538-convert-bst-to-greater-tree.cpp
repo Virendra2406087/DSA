07215e3610cf693d30b3ac4f58ca352d66927cd5
Virendra2406087 <virendrakumar993177@gmail.com>
2026-03-20 18:31:33 +0530

    Time: 3 ms (8.35%), Space: 35.4 MB (7.63%) - LeetHub

--- 0538-convert-bst-to-greater-tree/0538-convert-bst-to-greater-tree.cpp
+++ 0538-convert-bst-to-greater-tree/0538-convert-bst-to-greater-tree.cpp
@@ -11,46 +11,44 @@
  */
 class Solution {
 public:
-    void inorderstore(TreeNode* root,vector<int>&inOrder){
-        if(root==NULL){
+    void storeInorder(TreeNode* root,vector<int>&inorderNode){
+        if(root==nullptr){
             return;
         }
-        inorderstore(root->left,inOrder);
-        inOrder.push_back(root->val);
-        inorderstore(root->right,inOrder);
-        return;
+        storeInorder(root->left,inorderNode);
+        inorderNode.push_back(root->val);
+        storeInorder(root->right,inorderNode);
     }
-    void updateTree(TreeNode* root,vector<int>&inOrder,int &index){
-        if(root==NULL){
+    void updateTree(TreeNode* root,vector<int>&ans,int &index){
+        if(root==nullptr){
             return;
         }
-        updateTree(root->left,inOrder,index);
-        root->val=inOrder[index];
+        updateTree(root->left,ans,index);
+        root->val=ans[index];
         index++;
-        updateTree(root->right,inOrder,index);
-        return;
+        updateTree(root->right,ans,index);
     }
     TreeNode* convertBST(TreeNode* root) {
-         if(root==NULL){
-            return NULL;
+        if(root==nullptr){
+            return nullptr;
         }
-        if(root->left ==NULL && root->right ==NULL){
+        if(root->left == nullptr && root->right == nullptr){
             return root;
         }
-        vector<int>inOrder;
-        inorderstore(root,inOrder);
-        int n=inOrder.size();
+        vector<int>inorder;
+        storeInorder(root,inorder);
+        int n=inorder.size();
         for(int i=n-1;i>=0;i--){
-            int currVal=inOrder[i];
-            int nextVal=0;
-            if(i+1<n){
-                nextVal=inOrder[i+1];
-            }
-            int sum=currVal+nextVal;
-            inOrder[i]=sum;
+           int currVal=inorder[i];
+           int nextVal=0;
+           if(i+1<n){
+            nextVal=inorder[i+1];
+           }
+           int sum=currVal+nextVal;
+           inorder[i]=sum;
         }
         int index=0;
-        updateTree(root,inOrder,index);
+        updateTree(root,inorder,index);
         return root;
     }
 };
