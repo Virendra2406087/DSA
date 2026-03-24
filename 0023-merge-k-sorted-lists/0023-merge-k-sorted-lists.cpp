27fadfb0aa1149e8ce2e1f83cc72c111c33f9a6b
Virendra2406087 <virendrakumar993177@gmail.com>
2026-03-24 23:12:28 +0530

    Time: 0 ms (100%), Space: 18.3 MB (92.12%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -9,36 +9,37 @@
  * };
  */
 
-class Compare{
+class compare{
     public:
     bool operator()(ListNode* a,ListNode* b){
-        return a->val>b->val;
+        return a->val > b->val;
     }
-}; 
+};
+
 class Solution {
 public:
     ListNode* mergeKLists(vector<ListNode*>& lists) {
-        priority_queue<ListNode*,vector<ListNode*>,Compare>pq;
-        ListNode* head=NULL;
-        ListNode* tail=NULL;
-        int n=lists.size();
-        for(int i=0;i<n;i++){
+        priority_queue<ListNode*,vector<ListNode*>,compare>pq;
+        ListNode*head=nullptr;
+        ListNode* tail=nullptr;
+        int totalRows=lists.size();
+        for(int i=0;i<totalRows;i++){
             ListNode* first=lists[i];
-            if(first != NULL){
+            if(first != nullptr){
                 pq.push(first);
             }
         }
         while(!pq.empty()){
             ListNode* front=pq.top();
             pq.pop();
-            if(head == NULL && tail == NULL){
+            if(head==nullptr && tail==nullptr){
                 head=front;
                 tail=front;
             }else{
                 tail->next=front;
                 tail=front;
             }
-            if(front->next != NULL){
+            if(front->next != nullptr){
                 pq.push(front->next);
             }
         }
