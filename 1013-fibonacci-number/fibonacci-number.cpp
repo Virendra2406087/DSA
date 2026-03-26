316c5c6c7d00eca986d853a01c45c705ae874e8f
Virendra2406087 <virendrakumar993177@gmail.com>
2026-03-26 19:49:46 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (15.88%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -16,9 +16,28 @@ public:
         dp[n]= solveUsingMem(n-1,dp)+solveUsingMem(n-2,dp);
         return dp[n];
     }
+    int solveUsingTab(int n){
+        //1. 1-D dp
+
+        vector<int>dp(n+1);
+        //2. base case
+        if(n==0){
+            return 0;
+        }
+        if(n==1){
+            return 1;
+        }
+         dp[0]=0;
+         dp[1]=1;
+        for(int i=2;i<=n;i++){
+          int ans=dp[i-1]+dp[i-2];
+          dp[i]=ans;
+        }
+        return dp[n];
+
+    }
     int fib(int n) {
-        vector<int>dp(n+1,-1);
-        int ans=solveUsingMem(n,dp);
+        int ans=solveUsingTab(n);
         return ans;
     }
 };
