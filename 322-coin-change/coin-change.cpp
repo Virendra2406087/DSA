b213817cc2320318aa8b71980d36a568da7552c3
Virendra2406087 <virendrakumar993177@gmail.com>
2026-03-27 19:20:37 +0530

    Time: 35 ms (50.75%) | Memory: 19 MB (53.57%) - LeetSync

---
+++ 322-coin-change/coin-change.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+     int solve(vector<int>& coins,int amount){
+        if(amount==0){
+            return 0;
+        }
+        int mini=INT_MAX;
+        for(int i=0;i<coins.size();i++){
+            if(coins[i]<=amount){
+                int recursionAns=solve(coins,amount-coins[i]);
+                if(recursionAns != INT_MAX){
+                    mini=min(mini,1+recursionAns);
+                }
+            }
+        }
+        return mini;
+     }
+     int solveUsingMem(vector<int>& coins,int amount,vector<int>&dp){
+        if(amount==0){
+            return 0;
+        }
+        if(dp[amount] != -1){
+            return dp[amount];
+        }
+        int mini=INT_MAX;
+        for(int i=0;i<coins.size();i++){
+            if(coins[i]<=amount){
+                int recursionAns=solveUsingMem(coins,amount-coins[i],dp);
+                if(recursionAns != INT_MAX){
+                    mini=min(mini,1+recursionAns);
+                }
+            }
+        }
+        dp[amount]= mini;
+        return dp[amount];
+     }
+    int coinChange(vector<int>& coins, int amount) {
+        int n=coins.size();
+        vector<int>dp(amount+1,-1);
+        int ans=solveUsingMem(coins,amount,dp);
+        if(ans==INT_MAX){
+            return -1;
+        }
+        return ans;
+    }
+};
