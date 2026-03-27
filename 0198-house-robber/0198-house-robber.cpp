07f1514c773ec9143865750df80dce58692ccfa1
Virendra2406087 <virendrakumar993177@gmail.com>
2026-03-27 18:55:19 +0530

    Time: 0 ms (100%), Space: 10.7 MB (21.38%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,28 +1,26 @@
 class Solution {
 public:
-    int solveUsingRec(vector<int>&nums,int index){
+    int solve(vector<int>&nums,int index){
         if(index>=nums.size()){
             return 0;
         }
-        int include=nums[index]+solveUsingRec(nums,index+2);
-        int exclude=0+solveUsingRec(nums,index+1);
-        int ans=max(include,exclude);
-        return ans;
+        int include=nums[index]+solve(nums,index+2);
+        int exclude=0+solve(nums,index+1);
+        return max(include,exclude);
     }
     int solveUsingMem(vector<int>&nums,int index,vector<int>&dp){
         if(index>=nums.size()){
             return 0;
         }
-        if(dp[index] !=-1){
+        if(dp[index] != -1){
             return dp[index];
         }
         int include=nums[index]+solveUsingMem(nums,index+2,dp);
         int exclude=0+solveUsingMem(nums,index+1,dp);
-        int ans=max(include,exclude);
-        dp[index]=ans;
+        dp[index]= max(include,exclude);
         return dp[index];
     }
-    int solveUsingTab(vector<int>&nums){
+    int solveUsingTab(vector<int>&nums,int index){
         int n=nums.size();
         vector<int>dp(n+1,-1);
         dp[n]=0;
@@ -32,35 +30,16 @@ public:
                 temp=dp[i+2];
             }
             int include=nums[i]+temp;
-            int exclude=0+dp[i+1];
+            int exclude=dp[i+1];
             dp[i]=max(include,exclude);
-
         }
         return dp[0];
-    }
-    int solveUsingTabSO(vector<int>&nums){
-        int n=nums.size();
-        int next1=0;
-        int next2=0;
-        int curr;
-        for(int i=n-1;i>=0;i--){
-            int temp=0;
-            if(i+2<=n){
-                temp=next2;
-            }
-            int include=nums[i]+temp;
-            int exclude=0+next1;
-            curr=max(include,exclude);
-            next2=next1;
-            next1=curr;
 
-        }
-        return curr;
     }
     int rob(vector<int>& nums) {
-        // int n=nums.size();
-        // vector<int>dp(n+1,-1);
-        int ans=solveUsingTabSO(nums);
+        int n=nums.size();
+        vector<int>dp(n+1,-1);
+        int ans=solveUsingTab(nums,0);
         return ans;
     }
 };
