71d3ce7ec2fbbbe3886f000bafcd139a7986e6ec
Virendra2406087 <virendrakumar993177@gmail.com>
2026-03-27 20:40:32 +0530

    Time: 291 ms (26.48%) | Memory: 296.6 MB (5.76%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -26,10 +26,24 @@ public:
         dp[curr][prev+1] = max(include,exclude);
         return dp[curr][prev+1];
     }
-    int lengthOfLIS(vector<int>& nums) {
+    int solveUsingTab(vector<int>&nums){
         int n=nums.size();
-        vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
-        int ans=solveUsingMem(nums,0,-1,dp);
+        vector<vector<int>>dp(n+1,vector<int>(n+1,0));
+        for(int curr=n-1;curr>=0;curr--){
+            for(int prev=curr-1;prev>=-1;prev--){
+                int include=0;
+                if(prev ==-1 || nums[curr]>nums[prev]){
+                   include=1+dp[curr+1][curr+1];
+                }
+                int exclude=dp[curr+1][prev+1];
+                dp[curr][prev+1] = max(include,exclude);
+            }
+        }
+
+        return dp[0][0];
+    }
+    int lengthOfLIS(vector<int>& nums) {
+        int ans=solveUsingTab(nums);
         return ans;
     }
 };
