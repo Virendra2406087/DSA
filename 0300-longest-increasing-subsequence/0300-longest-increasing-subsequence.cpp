2c1bcbbd63ebca1b420efe15cb473fb422916b1d
Virendra2406087 <virendrakumar993177@gmail.com>
2026-03-27 19:58:39 +0530

    Time: 595 ms (7.27%), Space: 296.6 MB (11.47%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,60 +1,35 @@
-#include<vector>
-#include <algorithm>
 class Solution {
 public:
-    int solveUsingRec(vector<int>& nums,int curr,int prev){
+    int solve(vector<int>&nums,int curr,int prev){
         if(curr>=nums.size()){
             return 0;
         }
-        int includeAns=0;
+        int include=0;
         if(prev ==-1 || nums[curr]>nums[prev]){
-            includeAns=1+solveUsingRec(nums,curr+1,curr);
+           include=1+solve(nums,curr+1,curr);
         }
-        int excludeAns=0+solveUsingRec(nums,curr+1,prev);
-        int finalAns=max(includeAns,excludeAns);
-        return finalAns;
+        int exclude=solve(nums,curr+1,prev);
+        return max(include,exclude);
     }
-    int solveUsingMem(vector<int>& nums,int curr,int prev,vector<vector<int>>&dp){
+    int solveUsingMem(vector<int>&nums,int curr,int prev,vector<vector<int>>&dp){
         if(curr>=nums.size()){
             return 0;
         }
-        if(dp[curr][prev+1] !=-1){
-           return dp[curr][prev+1];
+        if(dp[curr][prev+1] != -1){
+            return dp[curr][prev+1];
         }
-        int includeAns=0;
+        int include=0;
         if(prev ==-1 || nums[curr]>nums[prev]){
-            includeAns=1+solveUsingMem(nums,curr+1,curr,dp);
+           include=1+solveUsingMem(nums,curr+1,curr,dp);
         }
-        int excludeAns=0+solveUsingMem(nums,curr+1,prev,dp);
-        int finalAns=max(includeAns,excludeAns);
-        dp[curr][prev+1]=finalAns;
+        int exclude=solveUsingMem(nums,curr+1,prev,dp);
+        dp[curr][prev+1] = max(include,exclude);
         return dp[curr][prev+1];
     }
-    int solveUsingTab(vector<int>& nums){
-        int n=nums.size();
-        vector<vector<int>>dp(n+1,vector<int>(n+1,0));
-        // if(dp[curr][prev+1] !=-1){
-        //    return dp[curr][prev+1];
-        // }
-        for(int curr=n-1;curr>=0;curr--){
-            for(int prev=curr-1;prev>=-1;prev--){
-            int includeAns=0;
-            if(prev ==-1 || nums[curr]>nums[prev]){
-            includeAns=1+dp[curr+1][curr+1];
-        }
-            int excludeAns=0+dp[curr+1][prev+1];
-            int finalAns=max(includeAns,excludeAns);
-            dp[curr][prev+1]=finalAns;
-        }
-        }
-        return dp[0][0];
-    }
-
     int lengthOfLIS(vector<int>& nums) {
-        // vector<vector<int>>dp(n,vector<int>(n+1,-1));
-        int curr=0;
-        int prev=-1;
-        int ans=solveUsingTab(nums);
+        int n=nums.size();
+        vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
+        int ans=solveUsingMem(nums,0,-1,dp);
         return ans;
     }
 };
