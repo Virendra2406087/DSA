ef6c35d98a3f40f303e56a833b32b671d30aee0c
Virendra2406087 <virendrakumar993177@gmail.com>
2026-03-28 19:12:36 +0530

    Time: 15 ms (50.32%), Space: 13.3 MB (22.77%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
@@ -1,93 +1,51 @@
 class Solution {
 public:
-    static const int mod = 1e9 + 7;
-    int solveUsingRec(int n,int k, int target){
-        if(n<0 || target<0){
+    long long int mod = 1000000007;
+    int solve(int n,int k,int target){
+        if(n<0 || target <0){
             return 0;
         }
         if(n==0 && target==0){
             return 1;
         }
-        if(n==0 && target !=0){
+        if(n==0 && target != 0){
             return 0;
         }
-        if(n !=0 && target ==0){
+        if(n !=0 && target == 0){
             return 0;
         }
+        
         int ans=0;
-        for(int value=1;value<=k;value++){
-            ans=ans+solveUsingRec(n-1,k,target-value);
+        for(int val=0;val<=k;val++){
+            ans=ans+solve(n-1,k,target-val);
         }
         return ans;
-
     }
-    int solveUsingMem(int n,int k, int target,vector<vector<long long int>> &dp){
-        if(n<0 || target<0){
+    int solveUsingMem(int n,int k,int target,vector<vector<long long int>>&dp){
+        if(n<0 || target <0){
             return 0;
         }
         if(n==0 && target==0){
             return 1;
         }
-        if(n==0 && target !=0){
+        if(n==0 && target != 0){
             return 0;
         }
-        if(n !=0 && target ==0){
+        if(n !=0 && target == 0){
             return 0;
         }
-        if(dp[n][target] !=-1){
+        if(dp[n][target] != -1){
             return dp[n][target];
         }
         long long int ans=0;
-        for(int value=1;value<=min(k,target);value++){
-            ans = (ans + solveUsingMem(n-1, k, target-value, dp)) % mod;
-
+        for(int val=1;val<=k;val++){
+            ans=(ans%mod+solveUsingMem(n-1,k,target-val,dp))%mod;
         }
-        dp[n][target]=ans;
+        dp[n][target]= ans;
         return dp[n][target];
     }
-    int solveUsingTab(int n,int k,int target){
-        vector<vector<long long>>dp(n+1,vector<long long>(target+1,0));
-        dp[0][0]=1;
-        for(int a=1;a<=n;a++){
-            for(int t=1;t<=target;t++){
-                long long ans=0;
-                for(int value=1;value<=min(k,target);value++){
-                    long long temp=0;
-                    if(t-value >=0){
-                        temp=dp[a-1][t-value]%mod;
-                    }
-                    ans=(ans%mod+temp)%mod;
-                }
-                dp[a][t]=ans;
-            }
-        }
-        return dp[n][target];
-    }
-    int solveUsingTabSO(int n,int k,int target){
-        // vector<vector<long long>>dp(n+1,vector<long long>(target+1,0));
-        vector<int>prev(target+1,0);
-        vector<int>curr(target+1,0);
-        prev[0]=1;
-        for(int a=1;a<=n;a++){
-            for(int t=1;t<=target;t++){
-                long long ans=0;
-                for(int value=1;value<=min(k,target);value++){
-                    long long temp=0;
-                    if(t-value >=0){
-                        temp=prev[t-value]%mod;
-                    }
-                    ans=(ans%mod+temp)%mod;
-                }
-                curr[t]=ans;
-            }
-            prev=curr;
-        }
-        return prev[target];
-    }
-
     int numRollsToTarget(int n, int k, int target) {
-        // vector<vector<long long int>>dp(n+1,vector<long long int>(target+1,-1));
-        long long int ans=solveUsingTabSO(n,k,target);
-        return ans%mod;
+        vector<vector<long long int>>dp(n+1,vector<long long int>(target+1,-1));
+        return solveUsingMem(n,k,target,dp);
     }
 };
