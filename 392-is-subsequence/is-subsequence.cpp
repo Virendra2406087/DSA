b38b189cb112835f8c0d5d8f3a2946bd0c24f2a4
Virendra2406087 <virendrakumar993177@gmail.com>
2026-03-31 18:59:57 +0530

    Time: 113 ms (2.73%) | Memory: 285.2 MB (5.21%) - LeetSync

--- 392-is-subsequence/is-subsequence.cpp
+++ 392-is-subsequence/is-subsequence.cpp
@@ -13,7 +13,26 @@ public:
             return solve(s,t,i,j+1);
         }
     }
+    bool solveUsingMem(string s,string t,int i,int j,vector<vector<int>>&dp){
+        if(i>=s.length()){
+            return true;
+        }
+        if(j>=t.length()){
+            return false;
+        }
+        if(dp[i][j] != -1){
+            return dp[i][j];
+        }
+        if(s[i]==t[j]){
+          return  dp[i][j]= solveUsingMem(s,t,i+1,j+1,dp);
+        }else{
+           return dp[i][j]= solveUsingMem(s,t,i,j+1,dp);
+        }
+    }
     bool isSubsequence(string s, string t) {
-        return solve(s,t,0,0);
+        int n=s.length();
+        int m=t.length();
+        vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
+        return solveUsingMem(s,t,0,0,dp);
     }
 };
