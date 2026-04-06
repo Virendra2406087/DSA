a0aee629f17b44c9626b80e7f1f5471b8e32d152
Virendra2406087 <virendrakumar993177@gmail.com>
2026-04-06 18:46:34 +0530

    Time: 78 ms (54.27%), Space: 76.3 MB (8.61%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
@@ -1,28 +1,38 @@
 class Solution {
 public:
-    int solveUsingTabSO(string a,string b){
-        // vector<vector<int>>dp(a.length()+1,vector<int>(b.length()+1,0));
-        vector<int>curr(b.length()+1,0);
-        vector<int>next(b.length()+1,0);
-        for(int i=a.length()-1;i>=0;i--){
-            for(int j=b.length()-1;j>=0;j--){
-                int ans=0;
-                if(a[i]==b[j]){
-                  ans=1+next[j+1];
-                }else{
-                  ans=0+max(curr[j+1],next[j]);
-                }
-                curr[j]=ans;
-
-            }
-                next=curr;
+    int solve(string a,string b,int i,int j){
+        if(i >= a.length() || j >= b.length()){
+            return 0;
         }
-        return next[0];
+        int ans=0;
+        if(a[i]==b[j]){
+            ans=1+solve(a,b,i+1,j+1);
+        }else{
+            ans=0+max(solve(a,b,i+1,j),solve(a,b,i,j+1));
+        }
+        return ans;
+    }
+    int solveUsingMem(string &a,string& b,int i,int j,vector<vector<int>>&dp){
+        if(i >= a.length() || j >= b.length()){
+            return 0;
+        }
+        if(dp[i][j] != -1){
+            return dp[i][j];
+        }
+        int ans=0;
+        if(a[i]==b[j]){
+            ans=1+solveUsingMem(a,b,i+1,j+1,dp);
+        }else{
+            ans=0+max(solveUsingMem(a,b,i+1,j,dp),solveUsingMem(a,b,i,j+1,dp));
+        }
+        dp[i][j]= ans;
+        return dp[i][j];
     }
     int longestPalindromeSubseq(string s) {
-        string first=s;
-        reverse(s.begin(),s.end());
-        string second=s;
-        return solveUsingTabSO(first,second);
+        int n=s.length();
+        string b=s;
+        reverse(b.begin(),b.end());
+        vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
+        return solveUsingMem(s,b,0,0,dp);
     }
 };
