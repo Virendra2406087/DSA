b1c87482001485ee1581b4695d20052d6389ee2d
Virendra2406087 <virendrakumar993177@gmail.com>
2026-04-06 19:40:19 +0530

    Time: 9 ms (40.6%), Space: 16 MB (11.19%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -40,10 +40,36 @@ public:
         dp[i][j]= ans;
         return dp[i][j];
     }
+    int solveUsingTab(string & a,string &b){
+        int n=a.size();
+        int m=b.size();
+        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+        for(int j=0;j<m;j++){
+            dp[n][j]=m-j;
+        }
+        for(int j=0;j<n;j++){
+            dp[j][m]=n-j;
+        }
+        for(int i=n-1;i>=0;i--){
+            for(int j=m-1;j>=0;j--){
+                int ans=0;
+                if(a[i]==b[j]){
+                    ans=dp[i+1][j+1];
+                }else{
+                    int insert=1+dp[i][j+1];
+                    int deleted=1+dp[i+1][j];
+                    int replace=1+dp[i+1][j+1];
+                    ans=min(insert,min(deleted,replace));
+                }
+                dp[i][j]= ans;
+            }
+        }
+        return dp[0][0];
+    }
     int minDistance(string word1, string word2) {
         int n=word1.size();
         int m=word2.size();
         vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
-        return solveUsingMem(word1,word2,0,0,dp);
+        return solveUsingTab(word1,word2);
     }
 };
