ce67c33f1145a29095f5a85ce37dd3e90d988e13
Virendra2406087 <virendrakumar993177@gmail.com>
2026-05-28 22:40:49 +0530

    Time: 13 ms (12.09%), Space: 23.7 MB (5.16%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,45 +1,43 @@
 class Solution {
 public:
-    void topoOrder(int n,unordered_map<int,list<int>>& adjList,vector<int>&topoSort){
-    unordered_map<int,int>indegree;
-    queue<int>q;
-    for(auto i:adjList){
-        for(auto nbr:i.second){
-            indegree[nbr]++;
+    void topoSort(int n,vector<int>& topoOrder,unordered_map<int,list<int>>& adjList){
+        queue<int>q;
+        map<int,int>indegree;
+        for(auto i:adjList){
+            for(auto nbr:i.second){
+                indegree[nbr]++;
+            }
         }
-    }
-    for(int node=0;node<n;node++){
-        if(indegree[node]==0){
-            q.push(node);
+        for(int node=0;node<n;node++){
+            if(indegree[node]==0){
+                q.push(node);
+            }
         }
-    }
-    while(!q.empty()){
-        int front=q.front();
-        q.pop();
-        topoSort.push_back(front);
-        for(auto nbr:adjList[front]){
-            indegree[nbr]--;
-        if(indegree[nbr]==0){
-            q.push(nbr);
-           }
+        while(!q.empty()){
+            int frontNode=q.front();
+            q.pop();
+            topoOrder.push_back(frontNode);
+            for(auto nbr:adjList[frontNode]){
+                indegree[nbr]--;
+                if(indegree[nbr]==0){
+                    q.push(nbr);
+                }
+            }
         }
-    }
     }
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
         unordered_map<int,list<int>>adjList;
-        for(vector<int>i:prerequisites){
+        for(vector<int> i:prerequisites){
             int u=i[0];
             int v=i[1];
-            adjList[v].push_back(u);
+            adjList[u].push_back(v);
         }
-        vector<int>topoSort;
-        
-        topoOrder(numCourses,adjList,topoSort);
-        if(topoSort.size()==numCourses){
+        vector<int>topoOrder;
+        topoSort(numCourses,topoOrder,adjList);
+        if(topoOrder.size()==numCourses){
             return true;
         }else{
-           return false;
-
+            return false;
         }
     }
 };
