0eadcc30b0abd217a5b7f5b2ce5f9873e993dc49
Virendra2406087 <virendrakumar993177@gmail.com>
2026-05-29 18:21:26 +0530

    Time: 85 ms (29.52%), Space: 25.6 MB (36.59%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
-    bool isSafe(int newX,int newY,int row,int col, vector<vector<int>>& diff,int currX,int currY){
-        if(newX>=0 && newY>=0 && newX<row && newY<col && diff[currX][currY]<diff[newX][newY]){
+    bool isSafe(int newX,int newY,int row,int col,vector<vector<int>>& diff,int currX,int currY){
+        if(newX>=0 && newY>=0 && newX<row && newY <col && diff[currX][currY]<diff[newX][newY]){
             return true;
-        }else{
-            return false;
+        }else {
+           return false;
         }
     }
     int minimumEffortPath(vector<vector<int>>& heights) {
-        priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,greater<pair<int,pair<int,int>>>>mini;
         int row=heights.size();
         int col=heights[0].size();
+        priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,greater<pair<int,pair<int,int>>>>mini;
         vector<vector<int>>diff(row,vector<int>(col,INT_MAX));
         int destX=row-1;
         int destY=col-1;
@@ -29,16 +29,14 @@ public:
                 int newX=currX+dx[i];
                 int newY=currY+dy[i];
                 if(isSafe(newX,newY,row,col,diff,currX,currY)){
-                    int maxDiff = max(currDiff, abs(heights[currX][currY] - heights[newX][newY]));
-
+                    int maxDiff=max(currDiff,abs(heights[currX][currY]-heights[newX][newY]));
                     diff[newX][newY]=min(diff[newX][newY],maxDiff);
-                    if(newX!=destX || newY != destY){
+                    if(newX != destX || newY != destY){
                         mini.push({diff[newX][newY],{newX,newY}});
                     }
                 }
             }
         }
         return diff[destX][destY];
-
     }
 };
