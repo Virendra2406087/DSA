1436ccba3e6f85dab10ecb7681a395c1426f0013
Virendra2406087 <virendrakumar993177@gmail.com>
2026-05-31 14:57:26 +0530

    Time: 0 ms (100%), Space: 18.4 MB (31.59%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,24 +1,26 @@
 class Solution {
 public:
-   bool isSafe(int i,int j,int oldColor,vector<vector<int>>& image,vector<vector<int>>& ans,map<pair<int,int>,bool>& visited){
-    if(i>=0 && i<image.size() && j>=0 && j<image[0].size() && ans[i][j]==oldColor && !visited[{i,j}]){
-        return 1;
+    bool isSafe(int i,int j,int oldColor,vector<vector<int>>& image,vector<vector<int>>& ans,map<pair<int,int>,bool>& visited){
+        int n=image.size();
+        int m=image[0].size();
+        if(i>=0 && i<n && j>=0 && j<m && ans[i][j]==oldColor && ! visited[{i,j}]){
+           return true;
+        }
+        return false;
     }
-    return 0;
-   }
-   void dfs(int oldColor,int newColor,map<pair<int,int>,bool>& visited,vector<vector<int>>& ans,vector<vector<int>>& image,int sr,int sc){
-    visited[{sr,sc}]=1;
-    ans[sr][sc]=newColor;
-    int dx[]={-1,0,1,0};
-    int dy[]={0,-1,0,1};
-    for(int i=0;i<4;i++){
-        int newX=sr+dx[i];
-        int newY=sc+dy[i];
-        if(isSafe(newX,newY,oldColor,image,ans,visited)){
-            dfs(oldColor,newColor,visited,ans,image,newX,newY);
+    void dfs(int oldColor,int newColor,map<pair<int,int>,bool>& visited,vector<vector<int>>& ans,vector<vector<int>>& image,int sr,int sc){
+        visited[{sr,sc}]=1;
+        ans[sr][sc]=newColor;
+        int dx[]={-1,0,1,0};
+        int dy[]={0,1,0,-1};
+        for(int k=0;k<4;k++){
+            int newX=sr+dx[k];
+            int newY=sc+dy[k];
+            if(isSafe(newX,newY,oldColor,image,ans,visited)){
+                dfs(oldColor,newColor,visited,ans,image,newX,newY);
+            }
         }
     }
-   }
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
         vector<vector<int>>ans=image;
         int oldColor=image[sr][sc];
