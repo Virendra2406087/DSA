f4784268d314c7358ed35a0d69953a5a9766d46b
Virendra2406087 <virendrakumar993177@gmail.com>
2026-05-31 15:35:50 +0530

    Time: 0 ms (100%), Space: 17.1 MB (39.01%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -1,17 +1,15 @@
 class Solution {
 public:
-    bool isSafe(int x,int y,int n,int m){
-        return (x>=0 && x<n && y>=0 && y<m );
+    bool isSafe(int i,int j,int n,int m){
+        return (i>=0 && i<n && j>=0 && j<m);
     }
-    int orangesRotting(vector<vector<int>>& grid) {
-        int n=grid.size();
-        int m=grid[0].size();
-        vector<vector<int>>temp=grid;
+    int bfs(vector<vector<int>>& grid,int n,int m){
         queue<pair<pair<int,int>,int>>q;
+        vector<vector<int>>ans=grid;
         int minTime=0;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(temp[i][j]==2){
+                if(ans[i][j]==2){
                     q.push({{i,j},0});
                 }
             }
@@ -23,24 +21,29 @@ public:
             int tempY=frontNode.first.second;
             int frontTime=frontNode.second;
             int dx[]={-1,0,1,0};
-            int dy[]={0,-1,0,1};
+            int dy[]={0,1,0,-1};
             for(int i=0;i<4;i++){
                 int newX=tempX+dx[i];
                 int newY=tempY+dy[i];
-                if(isSafe(newX,newY,n,m) && temp[newX][newY] == 1){
+                if(isSafe(newX,newY,n,m) && ans[newX][newY]==1){
                     q.push({{newX,newY},frontTime+1});
-                    temp[newX][newY]=2;
+                    ans[newX][newY]=2;
                     minTime=max(minTime,frontTime+1);
                 }
             }
         }
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(temp[i][j]==1){
+                if(ans[i][j]==1){
                     return -1;
                 }
             }
         }
         return minTime;
+    }
+    int orangesRotting(vector<vector<int>>& grid) {
+        int n=grid.size();
+        int m=grid[0].size();
+        return bfs(grid,n,m);
     }
 };
