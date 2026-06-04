f7b08ca4adadc622336a6c10fada1aa08a1e4e6d
Virendra2406087 <virendrakumar993177@gmail.com>
2026-06-04 15:44:55 +0530

    Time: 91 ms (33.78%), Space: 49.2 MB (6.31%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.cpp
+++ 0743-network-delay-time/0743-network-delay-time.cpp
@@ -1,19 +1,20 @@
 class Solution {
 public:
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
-        vector<vector<pair<int,int>>>adjList(n+1);
+        vector<vector<pair<int,int>>>adj(n+1);
         for(auto i:times){
             int u=i[0];
             int v=i[1];
             int wt=i[2];
-            adjList[u].push_back({v,wt});
+            adj[u].push_back({v,wt});
         }
         using p=pair<int,int>;
         priority_queue<p,vector<p>,greater<p>>pq;
         pq.push({0,k});
-        int visitedCount=0;
+        int visitedNodeCount=0;
         unordered_map<int,bool>visited;
         int timeAns=0;
+
         while(!pq.empty()){
             auto top=pq.top();
             pq.pop();
@@ -23,19 +24,20 @@ public:
                 continue;
             }
             visited[currNode]=true;
-            visitedCount++;
+            visitedNodeCount++;
             timeAns=max(timeAns,currTime);
-            for(auto j:adjList[currNode]){
+            for(auto j:adj[currNode]){
                 int nbrNode=j.first;
                 if(visited[nbrNode]==false){
-                    int edgeKuparKaTime=j.second;
-                    pq.push({currTime+edgeKuparKaTime,nbrNode});
+                    int edgeKUparKatime=j.second;
+                    pq.push({currTime+edgeKUparKatime,nbrNode});
                 }
             }
         }
-        if(visitedCount==n){
+        if(visitedNodeCount==n){
             return timeAns;
+        }else{
+            return -1;
         }
-        return -1;
     }
 };
