2908df5c6b8275250692820e659acc016c31160a
Virendra2406087 <virendrakumar993177@gmail.com>
2026-06-04 15:08:43 +0530

    Time: 134 ms (89.4%), Space: 169 MB (85.07%) - LeetHub

--- 1192-critical-connections-in-a-network/1192-critical-connections-in-a-network.cpp
+++ 1192-critical-connections-in-a-network/1192-critical-connections-in-a-network.cpp
@@ -1,45 +1,52 @@
 class Solution {
 public:
-    void CountBridge(int src,int parent,vector<int>& tin,vector<int>& low,unordered_map<int,bool>& visited,int timer,vector<vector<int>>& ans,unordered_map<int,list<int>>& adj){
-        timer++;
-        visited[src]=true;
-        tin[src]=timer;
-        low[src]=timer;
-        for(auto nbr : adj[src]){
-           if(nbr==parent){
-            continue;
-           }else if(!visited[nbr]){
-            CountBridge(nbr,src,tin,low,visited,timer,ans,adj);
-            low[src]=min(low[src],low[nbr]);
-            if(low[nbr]>tin[src]){
-                vector<int>temp;
-                temp.push_back(src);
-                temp.push_back(nbr);
-                ans.push_back(temp);
-            }
-           }else{
-            low[src]=min(low[src],low[nbr]);
-           }
-        }
+    void countBridge(int src,int parent,vector<int>& tin,vector<int>& low,vector<bool>& visited,int& timer,vector<vector<int>>& ans,vector<vector<int>>& adj) {
 
+        visited[src] = true;
+        tin[src] = low[src] = timer++;
+
+        for (auto nbr : adj[src]) {
+
+            if (nbr == parent){
+                continue;
+            }
+
+            if (!visited[nbr]) {
+                countBridge(nbr, src, tin, low,visited, timer, ans, adj);
+                low[src] = min(low[src], low[nbr]);
+                if (low[nbr] > tin[src]) {
+                    ans.push_back({src, nbr});
+                }
+            } else {
+                low[src] = min(low[src], tin[nbr]);
+            }
+        }
     }
-    vector<vector<int>> criticalConnections(int n, vector<vector<int>>& connections) {
-        vector<vector<int>>ans;
-        int timer=0;
-        int src=0;
-        int parent=-1;
-        vector<int>tin(n);
-        vector<int>low(n);
-        unordered_map<int,bool>visited;
-        unordered_map<int,list<int>>adj;
-        for(auto i:connections){
-            int u=i[0];
-            int v=i[1];
+
+    vector<vector<int>> criticalConnections(int n,vector<vector<int>>& connections) {
+        vector<vector<int>> adj(n);
+
+        for (auto &edge : connections) {
+            int u = edge[0];
+            int v = edge[1];
+
             adj[u].push_back(v);
             adj[v].push_back(u);
         }
-        CountBridge(src,parent,tin,low,visited,timer,ans,adj);
+
+        vector<int> tin(n, -1);
+        vector<int> low(n, -1);
+        vector<bool> visited(n, false);
+
+        vector<vector<int>> ans;
+        int timer = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
+                countBridge(i, -1, tin, low,visited, timer, ans, adj);
+            }
+        }
+
         return ans;
-        
     }
 };
