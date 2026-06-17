c3a4b5f8afe3b82d244be87f5e8b60fe1e3c5a26
Virendra2406087 <virendrakumar993177@gmail.com>
2026-06-17 22:14:22 +0530

    Time: 4 ms (9.35%) | Memory: 22.9 MB (41.73%) - LeetSync

---
+++ 3044-minimum-operations-to-collect-elements/minimum-operations-to-collect-elements.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums, int k) {
+        unordered_set<int> seen;
+        int count = 0;
+        for (int i = nums.size() - 1; i >= 0; i--) {
+            count++;
+
+            if (nums[i] <= k){
+                seen.insert(nums[i]);
+            }
+
+            if (seen.size() == k){
+                return count;
+            }
+        }
+
+        return count;
+    }
+};
