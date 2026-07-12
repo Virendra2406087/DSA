f73e7b2d7c360a7f7156c2edadae61292d37ac07
Virendra2406087 <virendrakumar993177@gmail.com>
2026-07-12 23:08:30 +0530

    Time: 1 ms (68.36%) | Memory: 11.7 MB (21.85%) - LeetSync

--- 451-sort-characters-by-frequency/sort-characters-by-frequency.cpp
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.cpp
@@ -1,20 +1,26 @@
 class Solution {
 public:
+    typedef pair<char,int>P;
+    struct lambda{
+        bool operator()(P&p1,P&p2){
+            return p1.second < p2.second;
+        }
+    };
     string frequencySort(string s) {
-        unordered_map<char, int> mp;
-        for (char ch : s){
+        priority_queue<P,vector<P>,lambda>pq;
+        unordered_map<char,int>mp;
+        for(char &ch:s){
             mp[ch]++;
         }
-
-        vector<pair<char, int>> v(mp.begin(), mp.end());
-
-        sort(v.begin(), v.end(), [](auto &a, auto &b) {
-            return a.second > b.second;
-        });
-        string ans="";
-        for(auto &p : v){
-           ans.append(p.second, p.first);
+        for(auto &it:mp){
+            pq.push({it.first,it.second});
         }
-        return ans;
+        string result="";
+        while(!pq.empty()){
+            P temp=pq.top();
+            pq.pop();
+            result +=string(temp.second,temp.first);
+        }
+        return result;
     }
 };
