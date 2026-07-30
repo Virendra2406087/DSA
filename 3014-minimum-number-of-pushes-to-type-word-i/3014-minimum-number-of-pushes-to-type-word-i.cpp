89b3b9f9cbf6e84e17208a33926a4d05d7fa6a9c
Virendra2406087 <virendrakumar993177@gmail.com>
2026-07-30 09:27:55 +0530

    Time: 0 ms (100%), Space: 8.6 MB (23.66%) - LeetHub

--- 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.cpp
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.cpp
@@ -2,15 +2,16 @@ class Solution {
 public:
     int minimumPushes(string word) {
         int result=0;
-        unordered_map<int,int>mp;
+        vector<int>mp(26,0);
         int assign=2;
-        for(char c:word){
-            if(assign>9){
-                assign=2;
-            }
-            mp[assign]++;
-            result+=mp[assign];
-            assign++;
+        for(char &c:word){
+            mp[c-'a']++;
+        }
+        sort(mp.begin(),mp.end(),greater<int>());
+        for(int i=0;i<26;i++){
+            int freq=mp[i];
+            int press=(i/8+1);
+            result+=press*freq;
         }
         return result;
     }
