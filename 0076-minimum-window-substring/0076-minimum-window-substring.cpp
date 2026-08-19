ff91560e550ce8dc1cfcae7ee5f10c8243bf9219
Virendra2406087 <198935804+Virendra2406087@users.noreply.github.com>
2026-08-19 23:04:43 +0530

    Time: 15 ms (35.72%), Space: 11.6 MB (43.58%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,43 +1,44 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-        int len1=s.length();
-        int len2=t.length();
-        if(len1<len2){
+        int len1 = s.size();
+        int len2 = t.size();
+        if(len1 < len2){
             return {};
         }
-        int ansIndex=-1;
-        int ansLen=INT_MAX;
-        unordered_map<char,int>sMap;
-        unordered_map<char,int>tMap;
-        for(auto i:t){
-            tMap[i]++;
+        int ansLen = INT_MAX;
+        int ansIndex = -1;
+        unordered_map <char,int> sMap;
+        unordered_map <char,int> tMap;
+        for(int i = 0;i<len2;i++){
+            tMap[t[i]]++;
         }
         int i=0,j=0;
-        int count=0;
+        int count = 0;
         while(j<len1){
-            char ch=s[j];
-            sMap[ch]++;
-            if(sMap[ch]<=tMap[ch]){
+            sMap[s[j]]++;
+            if(sMap[s[j]] <= tMap[s[j]]){
                 count++;
             }
-            if(count==len2){
-                while(sMap[s[i]]>tMap[s[i]]){
+            if (count==len2){
+                while(sMap[s[i]]>tMap[s[i]]) {
                     sMap[s[i]]--;
                     i++;
                 }
-                int windowLen=j-i+1;
-                if(windowLen<ansLen){
+                int windowLen = j-i+1;
+                if(windowLen < ansLen){
                     ansLen=windowLen;
-                    ansIndex=i;
+                    ansIndex = i;
                 }
             }
             j++;
         }
-        if(ansIndex==-1){
+        if (ansIndex == -1) {
             return "";
-        }else{
+        } else {
             return s.substr(ansIndex,ansLen);
         }
+
+
     }
 };
