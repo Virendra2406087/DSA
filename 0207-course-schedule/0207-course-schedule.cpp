c50dbcda5e5e1633d532891c2d72d85d18e0cb25
Virendra2406087 <198935804+Virendra2406087@users.noreply.github.com>
2026-09-05 11:37:52 +0530

    Time: 10 ms (24.76%), Space: 22.8 MB (5.24%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,5 +1,36 @@
 class Solution {
 public:
+    bool helperDFS(int src, unordered_map<int,vector<int>>& adjList, unordered_map<int,bool>& visited, unordered_map<int,bool>& dfsTracker) {
+        visited[src]=true;
+        dfsTracker[src]=true;
+        for(auto nbr : adjList[src]) {
+            if(!visited[nbr]) {
+                bool ans = helperDFS(nbr, adjList, visited, dfsTracker);
+                if(ans == true) {
+                    return true;
+                }
+            } else  if(visited[nbr] == true && dfsTracker[nbr] == true) {
+                return true;
+            }
+        }
+        dfsTracker[src]=false;
+        return false;
+    }
+    
+    bool solveUsingDFS(int totalNode, unordered_map<int,vector<int>> &adjList) {
+        unordered_map<int,bool> visited;
+        unordered_map<int,bool> dfsTracker;
+        for(int src=0; src<totalNode; src++) {
+            if(!visited[src]) {
+                bool ans = helperDFS(src,adjList, visited,dfsTracker);
+                if(ans == true) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
     void solveUsingBFS(int n, unordered_map<int,vector<int>>& adjList,vector<int>& topoOrder) {
         unordered_map<int,int> indegree;
         for(auto i : adjList) {
@@ -30,14 +61,15 @@ public:
         for(auto i: prerequisites) {
             int u = i[0];
             int v = i[1];
-            adjList[u].push_back(v);
+            adjList[v].push_back(u);
         }
-        vector<int> topoOrder;
-        solveUsingBFS(numCourses,adjList,topoOrder);
-        if(topoOrder.size() == numCourses) {
-            return true;
-        } else {
-            return false;
-        }
+        // vector<int> topoOrder;
+        // solveUsingBFS(numCourses,adjList,topoOrder);
+        // if(topoOrder.size() == numCourses) {
+        //     return true;
+        // } else {
+        //     return false;
+        // }
+        return !solveUsingDFS(numCourses,adjList);
     }
 };
