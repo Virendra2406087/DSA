93237dc3e7892af4862927b2ca59419e91eaed7d
Virendra2406087 <198935804+Virendra2406087@users.noreply.github.com>
2026-09-07 20:50:29 +0530

    Time: 107 ms (5.11%) | Memory: 35.3 MB (5.01%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,49 +1,42 @@
 class Solution {
 public:
-    bool isSafe(int newX,int newY,vector<vector<char>> & grid,map<pair<int,int>,bool>& visited){
-        int n=grid.size();
-        int m=grid[0].size();
-        if(newX>=0 && newX<n &&
-   newY>=0 && newY<m &&
-   grid[newX][newY]=='1' &&
-   !visited[{newX,newY}]) {
+    bool isSafe(int newX,int newY,int row,int col,vector<vector<char>>& grid,map<pair<int,int>,bool>& visited) {
+        if(newX>=0 && newY >= 0 && newX < row && newY < col && !visited[{newX,newY}] && grid[newX][newY]=='1') {
             return true;
+        } else {
+            return false;
         }
-        return false;
     }
-    void bfs(int i,int j,vector<vector<char>>& grid,map<pair<int,int>,bool>& visited){
-        int n=grid.size();
-        int m=grid[0].size();
+    void solveUsingBFS(int i, int j ,int row, int col, vector<vector<char>>& grid, map<pair<int,int>,bool>& visited) {
         queue<pair<int,int>>q;
         q.push({i,j});
         visited[{i,j}]=true;
-        while(!q.empty()){
-            auto frontNode=q.front();
+        while(!q.empty()) {
+            auto frontEle = q.front();
             q.pop();
-            int tempX=frontNode.first;
-            int tempY=frontNode.second;
+            int tempX = frontEle.first;
+            int tempY = frontEle.second;
             int dx[]={-1,0,1,0};
             int dy[]={0,1,0,-1};
-            for(int k=0;k<4;k++){
-                int newX=tempX+dx[k];
-                int newY=tempY+dy[k];
-                if(isSafe(newX,newY,grid,visited)){
+            for(int i=0;i<4;i++){
+                int newX=tempX + dx[i];
+                int newY=tempY + dy[i];
+                if(isSafe(newX,newY,row,col,grid,visited)) {
                     q.push({newX,newY});
                     visited[{newX,newY}]=true;
                 }
             }
-
         }
     }
     int numIslands(vector<vector<char>>& grid) {
-        map<pair<int,int>,bool>visited;
+        int row = grid.size();
+        int col = grid[0].size();
+        map<pair<int,int>,bool> visited;
         int count=0;
-        int n=grid.size();
-        int m=grid[0].size();
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
+        for(int i=0;i<row;i++){
+            for(int j=0;j<col;j++){
                 if(!visited[{i,j}] && grid[i][j]=='1'){
-                    bfs(i,j,grid,visited);
+                    solveUsingBFS(i,j,row,col,grid,visited);
                     count++;
                 }
             }
