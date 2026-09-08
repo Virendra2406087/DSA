4778a1478e4dc15f9a32c7e0ec7d5f24eb9584f1
Virendra2406087 <198935804+Virendra2406087@users.noreply.github.com>
2026-09-08 18:56:07 +0530

    Time: 2 ms (30.53%), Space: 17.1 MB (28.23%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -1,49 +1,51 @@
 class Solution {
 public:
-    bool isSafe(int i,int j,int n,int m){
-        return (i>=0 && i<n && j>=0 && j<m);
+    bool isSafe(int newX,int newY,int row,int col) {
+        if(newX >= 0 && newY >= 0 && newX < row && newY < col) {
+            return true;
+        } else {
+            return false;
+        }
     }
-    int bfs(vector<vector<int>>& grid,int n,int m){
-        queue<pair<pair<int,int>,int>>q;
-        vector<vector<int>>ans=grid;
+    
+    int orangesRotting(vector<vector<int>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+        vector<vector<int>>temp=grid;
         int minTime=0;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(ans[i][j]==2){
+        queue<pair<pair<int,int>,int>>q;
+        for(int i=0; i < n; i++) {
+            for(int j=0; j < m; j++) {
+                if(grid[i][j]==2) {
                     q.push({{i,j},0});
                 }
             }
         }
-        while(!q.empty()){
-            auto frontNode=q.front();
+        int dx[] = {-1,0,1,0};
+        int dy[] = {0,1,0,-1};
+        while(!q.empty()) {
+            auto front = q.front();
             q.pop();
-            int tempX=frontNode.first.first;
-            int tempY=frontNode.first.second;
-            int frontTime=frontNode.second;
-            int dx[]={-1,0,1,0};
-            int dy[]={0,1,0,-1};
-            for(int i=0;i<4;i++){
-                int newX=tempX+dx[i];
-                int newY=tempY+dy[i];
-                if(isSafe(newX,newY,n,m) && ans[newX][newY]==1){
+            int tempX = front.first.first;
+            int tempY = front.first.second;
+            int frontTime=front.second;
+            for(int i=0;i<4;i++) {
+                int newX = tempX + dx[i];
+                int newY = tempY + dy[i];
+                if(isSafe(newX,newY,n,m) && temp[newX][newY]==1) {
+                    temp[newX][newY]=2;
                     q.push({{newX,newY},frontTime+1});
-                    ans[newX][newY]=2;
-                    minTime=max(minTime,frontTime+1);
+                    minTime = max(minTime,frontTime+1);
                 }
             }
         }
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(ans[i][j]==1){
+                if(temp[i][j]==1){
                     return -1;
                 }
             }
         }
         return minTime;
-    }
-    int orangesRotting(vector<vector<int>>& grid) {
-        int n=grid.size();
-        int m=grid[0].size();
-        return bfs(grid,n,m);
     }
 };
