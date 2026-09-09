0d9f21f484acb57b81be93e884458f897b70493f
Virendra2406087 <198935804+Virendra2406087@users.noreply.github.com>
2026-09-09 18:04:00 +0530

    Time: 0 ms (100%), Space: 30.3 MB (85.64%) - LeetHub

--- 1306-jump-game-iii/1306-jump-game-iii.cpp
+++ 1306-jump-game-iii/1306-jump-game-iii.cpp
@@ -5,19 +5,20 @@ public:
         unordered_map<int,bool>visited;
         q.push(start);
         visited[start]=1;
-        while(! q.empty()){
-            int front=q.front();
+        while(!q.empty()) {
+            int front = q.front();
             q.pop();
-            if(arr[front]==0){
+            if(arr[front]==0) {
                 return true;
-            }else{
-                int plus=front+arr[front];
-                int minus=front-arr[front];
-                if(plus>=0 && plus<arr.size() && visited[plus]==0){
+            }
+            else {
+                int plus = front+arr[front];
+                int minus = front-arr[front];
+                if(plus >= 0 && plus < arr.size() && visited[plus]==0) {
                     q.push(plus);
                     visited[plus]=1;
                 }
-                if(minus>=0 && minus<arr.size() && visited[minus]==0){
+                if(minus >= 0 && minus < arr.size() && visited[minus]==0) {
                     q.push(minus);
                     visited[minus]=1;
                 }
