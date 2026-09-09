8425127de798dcc69c0dc195a59479ba1d62bde8
Virendra2406087 <198935804+Virendra2406087@users.noreply.github.com>
2026-09-09 18:42:23 +0530

    Time: 107 ms (5.9%), Space: 49.1 MB (5.58%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.cpp
+++ 0743-network-delay-time/0743-network-delay-time.cpp
@@ -1,43 +1,43 @@
 class Solution {
 public:
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
-        vector<vector<pair<int,int>>>adj(n+1);
-        for(auto i:times){
-            int u=i[0];
-            int v=i[1];
-            int wt=i[2];
-            adj[u].push_back({v,wt});
+        vector<vector<pair<int,int>>> adjList(n+1);
+        for(auto i:times) {
+            int u = i[0];
+            int v = i[1];
+            int wt = i[2];
+            adjList[u].push_back({v,wt});
         }
-        using p=pair<int,int>;
-        priority_queue<p,vector<p>,greater<p>>pq;
+        using P = pair<int,int>;
+        priority_queue<P, vector<P>, greater<P>>pq;
         pq.push({0,k});
-        int visitedNodeCount=0;
+        int visitedNodeCount = 0;
         unordered_map<int,bool>visited;
-        int timeAns=0;
-
-        while(!pq.empty()){
-            auto top=pq.top();
+        int timeAns = 0;
+        while(! pq.empty()) {
+            auto topEle = pq.top();
             pq.pop();
-            int currTime=top.first;
-            int currNode=top.second;
-            if(visited[currNode]==true){
+            int currTime = topEle.first;
+            int currNode = topEle.second;
+            if(visited[currNode]==true) {
                 continue;
             }
             visited[currNode]=true;
             visitedNodeCount++;
-            timeAns=max(timeAns,currTime);
-            for(auto j:adj[currNode]){
-                int nbrNode=j.first;
-                if(visited[nbrNode]==false){
-                    int edgeKUparKatime=j.second;
-                    pq.push({currTime+edgeKUparKatime,nbrNode});
+            timeAns = max(timeAns,currTime);
+            for(auto j : adjList[currNode]) {
+                int nbrNode = j.first;
+                if(visited[nbrNode]==false) {
+                    int edgeUpperTime = j.second;
+                    pq.push({currTime + edgeUpperTime ,nbrNode});
                 }
             }
         }
-        if(visitedNodeCount==n){
+        if(visitedNodeCount == n) {
             return timeAns;
-        }else{
+        } else {
             return -1;
         }
+
     }
 };
