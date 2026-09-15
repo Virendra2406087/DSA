a779acf7eec93cf62d697a20c860593d39ce8136
Virendra2406087 <198935804+Virendra2406087@users.noreply.github.com>
2026-09-15 13:00:10 +0530

    Time: 14 ms (22.46%) | Memory: 7.9 MB (20.96%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,43 +1,12 @@
 class Solution {
 public:
     int solve(int n){
-        if(n==0 || n==1){
+        if(n==1 || n==0){
             return n;
         }
         return solve(n-1)+solve(n-2);
     }
-    int solveUsingMem(int n,vector<int>&dp){
-        if(n==0 || n==1){
-            return n;
-        }
-        if(dp[n] !=-1){
-           return dp[n];
-        }
-        dp[n]= solveUsingMem(n-1,dp)+solveUsingMem(n-2,dp);
-        return dp[n];
-    }
-    int solveUsingTab(int n){
-        //1. 1-D dp
-
-        vector<int>dp(n+1);
-        //2. base case
-        if(n==0){
-            return 0;
-        }
-        if(n==1){
-            return 1;
-        }
-         dp[0]=0;
-         dp[1]=1;
-        for(int i=2;i<=n;i++){
-          int ans=dp[i-1]+dp[i-2];
-          dp[i]=ans;
-        }
-        return dp[n];
-
-    }
     int fib(int n) {
-        int ans=solveUsingTab(n);
-        return ans;
+        return solve(n);
     }
 };
