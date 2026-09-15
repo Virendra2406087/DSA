a5673777ab7c7e80497b6971c3b445597f6e4289
Virendra2406087 <198935804+Virendra2406087@users.noreply.github.com>
2026-09-15 14:18:24 +0530

    Time: 29 ms (59.74%), Space: 19.1 MB (56.12%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,60 +1,44 @@
 class Solution {
 public:
-     int solve(vector<int>& coins,int amount){
-        if(amount==0){
+    int solve(vector<int>& coins, int amount){
+        if(amount == 0){
             return 0;
         }
         int mini=INT_MAX;
-        for(int i=0;i<coins.size();i++){
-            if(coins[i]<=amount){
-                int recursionAns=solve(coins,amount-coins[i]);
-                if(recursionAns != INT_MAX){
-                    mini=min(mini,1+recursionAns);
+        for(int i=0; i < coins.size(); i++){
+            if(coins[i] <= amount){
+                int recAns = solve(coins, amount-coins[i]);
+                if(recAns != INT_MAX){
+                    mini = min(mini, 1+recAns);
                 }
             }
         }
         return mini;
-     }
-     int solveUsingMem(vector<int>& coins,int amount,vector<int>&dp){
-        if(amount==0){
+    }
+    int solveUsingMem(vector<int>& coins, int amount,vector<int>& dp){
+        if(amount == 0){
             return 0;
         }
         if(dp[amount] != -1){
             return dp[amount];
         }
         int mini=INT_MAX;
-        for(int i=0;i<coins.size();i++){
-            if(coins[i]<=amount){
-                int recursionAns=solveUsingMem(coins,amount-coins[i],dp);
-                if(recursionAns != INT_MAX){
-                    mini=min(mini,1+recursionAns);
+        for(int i=0; i < coins.size(); i++){
+            if(coins[i] <= amount){
+                int recAns = solveUsingMem(coins, amount-coins[i],dp);
+                if(recAns != INT_MAX){
+                    mini = min(mini, 1+recAns);
                 }
             }
         }
-        dp[amount]= mini;
-        return dp[amount];
-     }
-    int solveUsingTab(vector<int>&coins,int amount){
-        int n=amount;
-        vector<int>dp(n+1,INT_MAX);
-        dp[0]=0;
-        for(int value=1;value<=amount;value++){
-           int mini=INT_MAX;
-           for(int i=0;i<coins.size();i++){
-                if(coins[i]<=value){
-                    int recursionAns=dp[value-coins[i]];
-                    if(recursionAns != INT_MAX){
-                        mini=min(mini,1+recursionAns);
-                }
-            }
-        }
-        dp[value]=mini; 
-        }
+        dp[amount] = mini;
         return dp[amount];
     }
     int coinChange(vector<int>& coins, int amount) {
-        int ans=solveUsingTab(coins,amount);
-        if(ans==INT_MAX){
+        int n = coins.size();
+        vector<int>dp(amount+1,-1);
+        int ans = solveUsingMem(coins, amount,dp);
+        if(ans == INT_MAX){
             return -1;
         }
         return ans;
