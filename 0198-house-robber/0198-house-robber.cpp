af46f64fdd9ecafdb5366d1ea2f66b416457361e
Virendra2406087 <198935804+Virendra2406087@users.noreply.github.com>
2026-09-15 13:34:41 +0530

    Time: 0 ms (100%), Space: 10.8 MB (23.61%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -20,9 +20,42 @@ public:
         dp[index]= max(include,exclude);
         return dp[index];
     }
+    int solveUsingTab(vector<int>& nums) {
+        int n=nums.size();
+        vector<int> dp(n+1,-1);
+        dp[n]=0;
+        for(int i=n-1;i>=0;i--){
+            int temp=0;
+            if(i+2<=n){
+                temp=dp[i+2];
+            }
+            int include=temp+nums[i];
+            int exclude=dp[i+1];
+            dp[i]= max(include,exclude);
+        }
+        return dp[0];
+    }
+    int solveUsingTabSO(vector<int>& nums) {
+        int n=nums.size();
+        int next1=0;
+        int next2=0;
+        int curr;
+        for(int i=n-1;i>=0;i--){
+            int temp=0;
+            if(i+2<=n){
+                temp=next2;
+            }
+            int include=temp+nums[i];
+            int exclude=next1;
+            curr= max(include,exclude);
+            next2=next1;
+            next1=curr;
+        }
+        return curr;
+    }
     int rob(vector<int>& nums) {
         int n=nums.size();
         vector<int>dp(n+1,-1);
-        return solveUsingMem(nums,0,dp);
+        return solveUsingTabSO(nums);
     }
 };
