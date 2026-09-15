ea6a18a42be4a4dc0167e0c424f6fb8d160b4d98
Virendra2406087 <198935804+Virendra2406087@users.noreply.github.com>
2026-09-15 13:05:46 +0530

    Time: 0 ms (100%), Space: 8.3 MB (11.28%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -16,8 +16,23 @@ public:
         dp[n] =solveUsingMem(n-1,dp)+solveUsingMem(n-2,dp);
         return dp[n];
     }
+    int solveUsingTab(int n){
+        if(n==0){
+            return 0;
+        }
+        if(n==1){
+            return 1;
+        }
+        vector<int>dp(n+1,-1);
+        dp[0]=0;
+        dp[1]=1;
+        for(int i=2;i<=n;i++){
+            dp[i] =dp[i-1]+dp[i-2];
+        }
+        return dp[n];
+    }
     int fib(int n) {
         vector<int>dp(n+1,-1);
-        return solveUsingMem(n,dp);
+        return solveUsingTab(n);
     }
 };
