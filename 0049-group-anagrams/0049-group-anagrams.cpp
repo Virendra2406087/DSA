902c6cb64c76ee0759f83a92bde9911726b38212
Virendra2406087 <198935804+Virendra2406087@users.noreply.github.com>
2026-09-20 11:47:56 +0530

    Time: 128 ms (5.03%), Space: 111.9 MB (5.02%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,17 +1,43 @@
 class Solution {
 public:
+    struct TrieNode{
+      unordered_map<int,TrieNode*>children;
+      vector<string>words;
+    };
+    TrieNode* getNode(){
+        TrieNode * temp= new TrieNode();
+        return temp;
+    }
+    void insert(TrieNode* root, vector<int>& freq, string &word) {
+        TrieNode * temp=root;
+        for(int i=0;i<26;i++){
+            int count=freq[i];
+            if(temp->children.find(count) == temp->children.end()) {
+                temp->children[count]=getNode();
+            }
+            temp=temp->children[count];
+        }
+        temp->words.push_back(word);
+    }
+    void solve(TrieNode* root,vector<vector<string>> & ans) {
+        if(root->words.size() > 0){
+            ans.push_back(root->words);
+        }
+        for(auto &i:root->children) {
+            solve(i.second,ans);
+        }
+    }
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-      vector<vector<string>>ans;
-      unordered_map<string,vector<string>>m;   
-      for(int i=0;i<strs.size();i++){
-        string original=strs[i];
-        string arrange=original;
-        sort(arrange.begin(),arrange.end());
-        m[arrange].push_back(original);
-      }
-      for(auto i:m){
-        ans.push_back(i.second);
-      }
-      return ans;
+      vector<vector<string>> ans;
+        TrieNode* root=getNode();
+        for(int i=0;i<strs.size();i++){
+            vector<int>freq(26,0);
+            for(char ch:strs[i]) {
+                freq[ch-'a']++;
+            }
+            insert(root,freq,strs[i]);
+        }
+        solve(root,ans);
+        return ans;
     }
 };
